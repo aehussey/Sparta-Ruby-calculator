e7aa8f2de722945bb6d27fb2a1cb2d805cbaffe5
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2019-04-30 15:47:17 +0100

    finished advanced calculator

--- app.rb
+++ app.rb
@@ -6,7 +6,22 @@ puts "Would you like to use the (a) advanced, (b) basic, (c) BMI or (d) Trip Cal
 type = gets.chomp
 
 if type == "a"
-  
+
+  puts "Would you like to do (a) square root or (b) power?"
+  new_type = gets.chomp
+  if new_type == "a"
+    puts "What Number would you like to know the square root of?"
+    answer = gets.chomp
+    float_answer = eval(answer).to_f
+    puts Math.sqrt float_answer
+
+  elsif new_type == "b"
+    puts "What Number would you like have raised?"
+    raise = gets.chomp.to_f
+    puts "To what power ould you like it raised"
+    power = gets.chomp.to_f
+    puts raise**power
+
   end
 
 
