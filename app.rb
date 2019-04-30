be1b4393ff56e2676a191b824837aaabd1635591
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2019-04-30 15:45:01 +0100

    finished basic calculator

--- app.rb
+++ app.rb
@@ -1,17 +1,32 @@
-puts "Enter your name"
-name = gets.chomp
-puts "Hello #{name}, welcome to the calculator"
+# puts "Enter your name"
+# name = gets.chomp
+# puts "Hello #{name}, welcome to the calculator"
 
-puts "Would you like to use the (a) advanced, (b) basic, (c) BMI or (d) Trip Calculator"
+puts "Would you like to use the (a) advanced, (b) basic, (c) BMI or (d) Trip Calculator?"
 type = gets.chomp
 
 if type == "a"
-  puts "a"
+  
+  end
+
 
 
 elsif type == "b"
  puts "b"
 
+ puts "Enter the 1st number of the equation you want solved"
+ num1 = gets.chomp
+ float_num1 = eval(num1).to_f
+ string_num1 = float_num1.to_s
+
+
+
+ puts "Enter the operator of the equation you want solved (Use + - * /)"
+ operator = gets.chomp
+ puts "Enter the 2nd number of the equation you want solved"
+ num2 = gets.chomp
+
+ puts "#{num1} #{operator} #{num2} = #{eval(string_num1 + operator + num2)}"
 
 elsif type == "c"
   puts "c"
