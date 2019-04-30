3caead3b56a0ecc3f531554a38cd2846387de7e1
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2019-04-30 15:12:33 +0100

    Moved the README in and set up the inital get and if clauses

---
+++ app.rb
@@ -0,0 +1,22 @@
+puts "Enter your name"
+name = gets.chomp
+puts "Hello #{name}, welcome to the calculator"
+
+puts "Would you like to use the (a) advanced, (b) basic, (c) BMI or (d) Trip Calculator"
+type = gets.chomp
+
+if type == "a"
+  puts "a"
+
+
+elsif type == "b"
+ puts "b"
+
+
+elsif type == "c"
+  puts "c"
+
+elsif type == "d"
+  puts "d"
+
+end
